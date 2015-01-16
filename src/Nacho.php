597ddf1a69df38a2afb2daf32898b633fc80cc4c
Robert Kuzma <robert@balavec.com>
2015-01-16 12:12:13 +0100

    New function and test.

--- src/Nacho.php
+++ src/Nacho.php
@@ -1,10 +1,15 @@
 <?php namespace Balavec\Nacho;
  
 class Nacho {
- 
+
   public function hasCheese($bool = true)
   {
     return $bool;
+  }
+
+  public function howMuchCheese($var = 100)
+  {
+    return $var;
   }
  
 }
--- tests/NachoTest.php
+++ tests/NachoTest.php
@@ -4,10 +4,16 @@ use Balavec\Nacho\Nacho;
  
 class NachoTest extends PHPUnit_Framework_TestCase {
  
-  public function testNachHasCheese()
+  public function testNachoHasCheese()
   {
     $nacho = new Nacho;
     $this->assertTrue($nacho->hasCheese());
+  }
+
+  public function testHowMuchCheese()
+  {
+    $nacho = new Nacho;
+    $this->assertEquals(100,$nacho->howMuchCheese());
   }
  
 }
